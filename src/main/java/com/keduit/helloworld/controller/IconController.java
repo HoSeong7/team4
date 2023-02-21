34fdf067af8e42dd64132328326c76a1700f102a
yu-teacher <yusm1231@gmail.com>
2023-02-21 17:40:47 +0900

    0221메인전체수정

---
+++ src/main/java/com/keduit/helloworld/controller/IconController.java
@@ -0,0 +1,34 @@
+package com.keduit.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Controller
+@RequestMapping("/icon/*")
+public class IconController {
+	
+	@GetMapping("/calculator")
+	public void calculator() {
+		log.info("IconController calculator");
+	}
+	
+	@GetMapping("/memo")
+	public void memo() {
+		log.info("IconController memo");
+	}
+	
+	@GetMapping("/tetris")
+	public void tetris() {
+		log.info("IconController tetris");
+	}
+	
+	@GetMapping("/compass")
+	public void compass() {
+		log.info("IconController compass");
+	}
+
+}
